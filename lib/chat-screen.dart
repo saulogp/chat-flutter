ffef3afcd7263da138943776f4ee19379b56d325
saulogp <sauloproetti@gmail.com>
2020-04-14 15:09:56 -0300

    ajustes id imagem

--- lib/chat-screen.dart
+++ lib/chat-screen.dart
@@ -73,7 +73,7 @@ class _ChatScreenState extends State<ChatScreen> {
     if (imgFile != null) {
       StorageUploadTask task = FirebaseStorage.instance
           .ref()
-          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .child( user.uid + DateTime.now().millisecondsSinceEpoch.toString())
           .putFile(imgFile);
 
       StorageTaskSnapshot taskSnapshot = await task.onComplete;
